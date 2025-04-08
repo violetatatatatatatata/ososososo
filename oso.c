4f649e813fda050970892d87b4d53538cc3b2be0
superpipiman <velandriaa9@gmail.com>
2025-04-08 12:50:29 +0200

    a

---
+++ oso.c
@@ -0,0 +1,13 @@
+/*******************************
+	PROYECTO DEL OSO
+********************************/
+
+#include "oso.h"
+
+int	leer_entrada() {
+	// aqui se usa scanf
+}
+
+int	print_tablero() {
+
+}
